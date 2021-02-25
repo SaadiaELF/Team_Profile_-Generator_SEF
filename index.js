ac8e5756db52f6d5d33627242882e6a66269f319
Saadia El fekak <saadiaelfekak@gmail.com>
2021-02-25 14:31:32 +0000

    Adding validation to inputs

--- index.js
+++ index.js
@@ -1,27 +1,42 @@
 // Packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+
 function promptQuestions(title, info) {
     inquirer.prompt([
         {
             type: 'input',
             message: `Please enter the ${title} name : `,
             name: `${title}Name`,
+            validate: function (input) {
+                return /^([a-zA-Z])$/.test(input)
+            }
         },
         {
             type: 'input',
             message: `Please enter the ${title} ID : `,
             name: `${title}Id`,
+            validate: function (input) {
+                return /^([a-zA-Z0-9])$/.test(input)
+            }
+
         },
         {
             type: 'input',
             message: `Please enter the ${title} email address : `,
             name: `${title}Email`,
+            validate: function (email) {
+                // Regex mail check (return true if valid mail)
+                return /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(email);
+            }
         },
         {
             type: 'input',
             message: `Please enter the ${info} : `,
             name: `${info}`,
+            validate: function (input) {
+                return /^([a-zA-Z0-9])$/.test(input)
+            }
         },
     ])
         .then(function (data) {
@@ -53,6 +68,11 @@ function addEmployee() {
             })
 }
 
-
+function validateInput(input) {
+    if (typeof input !== 'string') {
+        return 'Please enter a string';
+    }
+    return true;
+}
 // function to initialize app 
 promptQuestions("Manager", "office Number");
